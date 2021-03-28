d8392369460569d559f40b931632e29f41ea6e5f
whitejh <kei02012@naver.com>
2021-03-28 16:00:23 +0900

    Todo App vue프로젝트 구현

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <span v-bind:class="{ textCompleted: todoItem.textCompleted }">
+          <i class="checkBtn fas fa-check" v-bind:class="{ checkBtnCompleted: todoItem.completed }" v-on:click="toggleComplete(todoItem, index)"></i>
+          <span v-bind:class="{ textCompleted: todoItem.completed }">{{ todoItem.item }}</span>
+        </span>
+
+        <span class="removeBtn" type="button" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: [],
+    };
+  },
+  methods: {
+    removeTodo: function(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(todoItem, index) {
+      console.log(todoItem, index);
+      todoItem.completed = !todoItem.completed;
+      // 로컬 스토리지의 데이터를 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    sortObj: function(a, b) {
+      if (a.item < b.item) {
+        return -1;
+      }
+      if (a.item > b.item) {
+        return 1;
+      }
+      return 0;
+    },
+  },
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+      this.todoItems.sort(this.sortObj); // 배열의 sort API 사용
+    }
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted {
+  color: #b3adad;
+}
+
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
